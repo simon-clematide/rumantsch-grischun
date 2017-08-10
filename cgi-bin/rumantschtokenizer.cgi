6ea37e90e91e434dbc7e29a7ffb747beaa5dad09
Simon Clematide <simon.clematide@uzh.ch>
2017-08-11 00:04:34 +0200

    add first version of tokenizer web interface

---
+++ cgi-bin/rumantschtokenizer.cgi
@@ -0,0 +1,60 @@
+#!/usr/bin/perl -CSD
+use File::Spec;
+my ($volume, $CGIDIR, $file) = File::Spec->splitpath(__FILE__);
+print STDERR "$volume $CGIDIR $file\n";
+use CGI qw(:standard);
+use CGI::Carp qw(fatalsToBrowser);
+use Encode qw(decode encode);
+
+# Some Variables
+$contenttype = "Content-Type: text/plain; charset=utf-8\n\n";
+$characterlimit = 1000000;
+$Rawtext = param('rohtext');
+
+$tmpdatei = "/tmp/$file.$$";
+$Program = $CGIDIR . 'tools/parse.sh';
+
+@Arguments =  ( $tmpdatei,  $Mode) ;
+$ENV{'PATH'} = "$CGIDIR:$CGIDIR/tools:/mnt/storage/clfiles/resources/bin/:$ENV{PATH}";
+
+$Rawtext = decode("utf-8", $Rawtext);
+$Rawtext =~ tr/\r/\n/s;
+
+$rawtextlength = length($Rawtext);
+if ($rawtextlength>$characterlimit) {
+print $contenttype;
+print "Sorry, input is limited to $characterlimit characters!\n";
+exit(0);
+};
+
+if ($rawtextlength < 1) {
+	print $contenttype;
+	print  "ERROR: Sorry, not enough input!\n" ;
+	exit;
+}
+;
+
+$Rawtext =~ s/\r//g;
+
+die "Could not create file\n" unless open(TMPFILE,"> $tmpdatei");
+print TMPFILE $Rawtext;
+close(TMPFILE);
+
+
+undef $/ ;
+die "Keine Datei $CGIDIR/ vorhanden\n" unless -x "$CGIDIR/$file";
+die "cannot fork: $!" unless defined($pid = open(SICHERES_KIND, "-|"));
+if ($pid == 0) {
+  exec("perl $CGIDIR/tools/tokenizer.pl $tmpdatei /dev/stdout")
+	or die "Kann $! $?  nicht ausfuehren: $!";
+} else {
+  undef $/ ;
+#die "Verarbeitet\n";
+  print "Content-Type: text/plain; charset=utf-8
+
+";
+  while (<SICHERES_KIND>) { print };
+
+  close(SICHERES_KIND);
+}
+unlink $tmpdatei;
---
+++ cgi-bin/tools/tokenizer.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+# University of Zurich - Institute of Computational Linguistics
+# Morphology anlysis for Romansh: Tokenizer
+# Author: Reto Baumgartner
+# Date: June 2013
+
+#use: perl tokenizer.pl infile outfile
+
+#use warnings;
+use utf8;
+use strict;
+
+my @tokens;
+my $text;
+chomp @ARGV;
+
+open (INFILE, '<:encoding(UTF-8)', "$ARGV[0]") || die "kann $ARGV[0] nicht öffnen.";
+while (<INFILE>){
+    $text .= "$_";
+}
+close (INFILE);
+
+# Satzzeichen abtrennen:
+$text =~ s/([;:?!])/ \1/g;
+#
+$text =~ s/(\D)\,/\1 \,/g;
+$text =~ s/(\d)\,(\s)/\1 \,\2/g;
+#
+$text =~ s/([a-zäöüàèìòùâêîôûáéíóú»"“‘›])(\.)\s([A-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ])/\1 \. \3/g;
+$text =~ s/([a-zäöüàèìòùâêîôûáéíóú»"“‘›])(\.)\n/\1 \. \3/g;
+# Vier Ziffern sind meistens Jahresangaben.
+$text =~ s/(\d\d\d\d)(\.)/\1 \2/g;
+# Apostrophierung behandeln
+## Es wird nur abgetrennt, wenn der Apostroph zum ersten Teil gehoert. 
+## Ist ein Vokal vom nachfolgenden Teil elidiert, gilt dies als Endung.
+$text =~ s/([a-zäöüàèìòùâêîôûáéíóúA-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ])('|’)(h?[aeiouyäöüàèìòùâêîôûáéíóú])/\1\2 \3/ig;
+# Klammern einzeln stehen lassen:
+$text =~ s/([({\[])/ \1 /g;
+$text =~ s/([)}\]])/ \1 /g;
+# Anfuehrungszeichen abtrennen
+$text =~ s/([^a-zäöüàèìòùâêîôûáéíóúA-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ])([«"„‚‹])([a-zäöüàèìòùâêîôûáéíóúA-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ])()/\1 \2 \3/g;
+$text =~ s/([a-zäöüàèìòùâêîôûáéíóúA-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ?!.])([»"“‘›])([^a-zäöüàèìòùâêîôûáéíóúA-ZÄÖÜÀÈÌÒÙÂÊÎÔÛÁÉÍÓÚ])/\1 \2 \3/g;
+# Abkürzungen behandeln
+$text =~ s/(usw|etc) \./\1\./g;
+$text =~ s/(\s)+/ /g;
+$text =~ s/(\n)+/ /g;
+
+
+@tokens = split(/\s/, $text); 
+
+open (OUTFILE, '>:encoding(UTF-8)', "$ARGV[1]") || die "kann $ARGV[1] nicht öffnen.";
+foreach (@tokens) {print OUTFILE "$_\n";}
+close (OUTFILE);
