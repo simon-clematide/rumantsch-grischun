6663c0f72282ea59f15414895394b1c0df54e81b
SC via clrumantsch <siclemat@ifi.uzh.ch>
2018-01-29 17:10:22 +0100

    improve sequence tagger

--- cgi-bin/tools/analyse.py
+++ cgi-bin/tools/analyse.py
@@ -40,6 +40,24 @@ def betterPrint(token, analysis):
     else:
         print(token + "\t" + analysis)
 
+def reweight_pos_by_morpho(entry):
+    """
+    [[u'gudogna', u'gudagnar+Verb+PresInd+1P+Sg^|+Pron+Pers+Nom+1P+MF+Sg', 0.198458, u'+Adv'], [u'gudogna', u'gudagnar+Verb+PresInd+3P+Sg', 0.198458, u'+Adv
+458, u'+Adv']]
+
+    scale the probabilites in order to reflect typical prominency
+     - 3P > 2P > 1P
+     - PresInd > Impv
+
+    """
+
+    if '+2P' in entry[1]:
+        entry[2] = entry[2]*0.99
+    if '+1P' in entry[1]:
+        entry[2] = entry[2]*0.99
+    if '+Impv' in entry[1]:
+        entry[2] = entry[2]*0.99
+
 def processSentence(sentence, lastsentence, options=None):
     # Collect output from morphology analysis:
     if options.debug:
@@ -67,7 +85,8 @@ def processSentence(sentence, lastsentence, options=None):
     poswords.append(poswordsmulti[0].split("\n")[1:])
     poswords.append(poswordsmulti[1].split("\n")[1:])
     poswords.append(poswordsmulti[2].split("\n")[1:])
-
+    if options.debug:
+        print('#WAPITI',poswords,file=sys.stderr)
     wordcounter = 0
     # Compare results
     for i,mw in enumerate(morphowords):
@@ -80,50 +99,74 @@ def processSentence(sentence, lastsentence, options=None):
 
         morphocandidates = mw.split("\n")
 
-        for p in poswords:
+        for j,p in enumerate(poswords):
             postoken = p[i].split("\t")[0]
             postagging = p[i].split("\t")[1]
             postags = postagging.split("+")[1:]
-            posscore = eval(p[i].split("\t")[2].split("/")[1])
+            if options.debug:
+                print('#POSTAGS',postags,file=sys.stderr)
+            posscore = float(p[i].split("\t")[2].split("/")[1])
 
             for m in morphocandidates:
                 if m == '': continue
                 morphotoken, morphoanalysis = m.split("\t", 1)
                 morpholemma, morphotagging = morphoanalysis.split("+", 1)
                 morphotags = morphotagging.split("+")
-
+                if options.debug and j < 1:
+                    print('#MORPHOTAGS',morphotags,file=sys.stderr)
+                entry = [postoken, morphoanalysis, posscore, postagging]
                 if morphoanalysis == "+?":
-                    notmatching.append((postoken, morphoanalysis, posscore, postagging))
+                    notmatching.append(entry)
+                    if options.debug:
+                        print('#NOENTRY',entry, file=sys.stderr)
                 elif set(postags).issubset(morphotags):
-                    matching.append((postoken, morphoanalysis, posscore, postagging))
+                    matching.append(entry)
                 elif len(set(postags)-set(morphotags)) == 1:
-                    matchingminus1.append((postoken, morphoanalysis, posscore, postagging))
+
+                    reweight_pos_by_morpho(entry)
+                    matchingminus1.append(entry)
+                    if options.debug :
+                        print('#MINUS1',set(postags),set(morphotags),file=sys.stderr)
                 elif len(set(postags)-set(morphotags)) == 2:
-                    matchingminus2.append((postoken, morphoanalysis, posscore, postagging))
+                    reweight_pos_by_morpho(entry)
+                    matchingminus2.append(entry)
+                    if options.debug:
+                        print('#MINUS2',set(postags),set(morphotags),file=sys.stderr)
                 else:
-                    notmatching.append((postoken, morphoanalysis, posscore, postagging))
+                    notmatching.append(entry)
                     if options.debug:
                         print('notmatching', notmatching, file=sys.stderr)
 
         # Choose and print the best candidate
         if len(matching) > 0:
-            best = sorted(matching, key=lambda m: m[2])
-            betterPrint(best[-1][0], best[-1][1])
+            if options.debug:
+                print('#MATCHING A BEFORE SORTING',matching, file=sys.stderr)
+            matching.sort(key=lambda m: m[2])
+            betterPrint(matching[-1][0], matching[-1][1])
+            if options.debug:
+                print('#MATCHING A',matching[-1][0], matching[-1][1],file=sys.stderr)
 
         elif len(matchingminus1) > 0:
-            best = sorted(matchingminus1, key=lambda m: m[2])
-            betterPrint(best[-1][0], best[-1][1])
+            if options.debug:
+                print('#MATCHING B BEFORE SORTING',matchingminus1, file=sys.stderr)
+            matchingminus1.sort(key=lambda m: m[2])
+            betterPrint(matchingminus1[-1][0], matchingminus1[-1][1])
+            if options.debug:
+                print('#MATCHING B',matchingminus1[-1][0], matchingminus1[-1][1],file=sys.stderr)
 
         elif len(matchingminus2) > 0:
-            best = sorted(matchingminus2, key=lambda m: m[2])
-            betterPrint(best[-1][0], best[-1][1])
+            matchingminus2.sort(key=lambda m: m[2])
+            betterPrint(matchingminus2[-1][0], matchingminus2[-1][1])
+            if options.debug:
+                print('#MATCHING C',matchingminus2[-1][0], matchingminus2[-1][1],file=sys.stderr)
 
         else:
+            notmatching.sort( key=lambda m: m[2])
             if notmatching[0][1] == "+?":
-                betterPrint(notmatching[-1][0], notmatching[-1][0] + notmatching[-1][3])
+                betterPrint(notmatching[-1][0], notmatching[-1][0] + notmatching[-1][3]+'+UNKNOWN')
             else:
-                best = sorted(notmatching, key=lambda m: m[2])
-                betterPrint(best[-1][0], best[-1][1])
+
+                betterPrint(notmatching[-1][0], notmatching[-1][1]+'+UNKNOWN')
         if blankLine and wordcounter == len(morphowords) and lastsentence:
             pass
         elif blankLine:
@@ -152,7 +195,8 @@ def main():
                       help='choose binary finite state analyzer transducer (%default)')
 
     (options, args) = parser.parse_args()
-    if options.debug: print >> sys.stderr, "options=",options
+    if options.debug:
+        print("options=",options, file=sys.stderr)
 
     if options.model != taggingmodel:
         taggingmodel = options.model
@@ -167,7 +211,7 @@ def main():
 
     # Process sentences in input
     sentences = input_file.read().split("\n\n")
-    if options.debug: print >>sys.stderr, sentences
+    if options.debug: print(sys.stderr, sentences, file=sys.stderr)
     sentencecounter = 0
     for s in sentences:
         s = s.strip()
